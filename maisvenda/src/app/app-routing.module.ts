8940fcb633ae46299cc8c3565d59c374905b13a1
Hamilton Vale <hamilton.valnet@gmail.com>
2023-04-12 15:50:35 -0300

    home component

--- maisvenda/src/app/app-routing.module.ts
+++ maisvenda/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { LoginComponent } from "./components/login/login.component";
+
 import { ConfigurarComponent } from "./components/configurar/configurar.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const routers:Routes =  [
+  { path:'', component:HomeComponent },
   { path:'dash', component:DashboardComponent },
   { path:'login', component:LoginComponent },
   { path:'config', component:ConfigurarComponent }
 ]
-
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routers)],
--- maisvenda/src/app/app.module.ts
+++ maisvenda/src/app/app.module.ts
@@ -9,39 +9,44 @@ import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 
+
+import { IconsModule } from './icons/icons.module';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { IconsModule } from './icons/icons.module';
-import { MatMenuModule } from '@angular/material/menu';
 import { LoginComponent } from './components/login/login.component';
 import { ConfigurarComponent } from './components/configurar/configurar.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ConfigurarComponent,
     DashboardComponent,
     HeaderComponent,
+    HomeComponent,
     LoginComponent,
-    ConfigurarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     FormsModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
     IconsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],

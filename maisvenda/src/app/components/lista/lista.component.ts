44740d8d099536be2bf66ba0cc2e61a84dac7f5d
Hamilton Vale <hamilton.valnet@gmail.com>
2023-04-16 20:59:21 -0300

    Storage Lojas

--- maisvenda/src/app/components/lista/lista.component.ts
+++ maisvenda/src/app/components/lista/lista.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from '@angular/core';
+import { ILojas } from 'src/app/interfaces/lojas';
 
 @Component({
   selector: 'app-lista',
@@ -7,14 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista.component.scss']
 })
 export class ListaComponent {
+  localStorage: any;
+
   title="Lista de compras"
   preco:any=''
+  Loja?:any
+  LojaNome?:string
+  id:number= 0
 
+  async selected(){
+    if(localStorage.getItem('Loja')){
+      this.Loja = await localStorage.getItem('Loja') || ""
+      let x = JSON.parse(this.Loja)
+      this.LojaNome = x.nome
+      this.id = x.id
+     }
+   }
 
   reverter(){
-
-
-   this.preco  =  this.preco.replace(/[^\d]+/gi,'');
+   this.preco  =  this.preco.replace(/[^\d]+/gi,'')
   }
 }
 

b076c1f52aad4c442b8298f846592eb76d59bd8f
Hamilton Vale <hamilton.valnet@gmail.com>
2023-05-01 10:36:31 -0300

    melhoria URL

--- maisvenda/src/app/service/produto.service.ts
+++ maisvenda/src/app/service/produto.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
-  private readonly API = 'https://api.cosmos.bluesoft.com.br/gtins/'
+  private readonly API = environment.URL_API
   constructor(private http: HttpClient) { }
 
   list(codigoPro:string){
--- maisvenda/src/environments/environment.ts
+++ maisvenda/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  token_cosmos:'ZyEluU2E2uP4G0zqjR00nA'
+  token_cosmos:'ZyEluU2E2uP4G0zqjR00nA',
+  URL_API:'https://api.cosmos.bluesoft.com.br/gtins/'
 };
